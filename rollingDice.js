219b619c241dcf83c5781a82e7add40f02ec2326
Konstantin Toroshchin <k.toroshchin@gmail.com>
2018-07-31 16:44:33 +0000

    Finished Code

---
+++ rollingDice.js
@@ -0,0 +1,12 @@
+//get user input
+var numbers = process.argv.slice(2);  //array
+var numberList = [];
+
+// number of times you have to roll
+for (var x = 0 ; x < numbers[0]; x++){
+     var randomNumber = Math.floor((Math.random() * 6) + 1);
+     // add random number to empty string
+     numberList.push(randomNumber);
+}
+
+console.log(numberList.join(", "));
